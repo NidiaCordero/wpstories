6455e1a53acecc8e86180e9d66d7ddb6f6359fec
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-08-21 10:25:46 +0200

    Restructuration de index vers templates

---
+++ templates/perso.php
@@ -0,0 +1,17 @@
+
+    <section class="ftco-section ftco-no-pt ftco-section-about ftco-no-pb bg-darken">
+    	<div class="container-fluid">
+    		<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-9 order-md-last img py-5" style="background-image: url(images/bg_3.jpg);"></div>
+
+	        <div class="col-sm-6 col-md-6 col-lg-3 py-4 text d-flex align-items-center ftco-animate">
+	        	<div class="text-2 py-5 px-4">
+	          	<p class="mb-5"><a href="https://vimeo.com/45830194" class="btn-custom popup-vimeo">Watch Video <span class="ion-ios-play ml-4"></span></a></p>
+	            <h1 class="mb-5">Roger <br> Bosch</h1>
+	            <p class="mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+	            <span class="signature">Roger.Bosch</span>
+	          </div>
+	        </div>
+    		</div>
+    	</div>
+    </section>
---
+++ templates/season.php
@@ -0,0 +1,69 @@
+<section class="ftco-section">
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-9">
+        		<div class="row">
+		          <div class="col-md-12 heading-section ftco-animate">
+		            <h2 class="mb-4"><span>Holiday Seasons Recipes</span></h2>
+		          </div>
+		        </div>
+        		<div class="row">
+        			<div class="col-md-6 col-lg-6 ftco-animate">
+		    				<div class="blog-entry">
+		    					<div class="img img-big img-big-2 d-flex align-items-end" style="background-image: url(images/image_1.jpg);">
+		    						<div class="overlay"></div>
+		    						<div class="text">
+		    							<span class="subheading">Food</span>
+		    							<h3><a href="#">ham sandwich on white surface</a></h3>
+		    							<p class="mb-0"><a href="#" class="btn-custom">Read More <span class="icon-arrow_forward ml-4"></span></a></p>
+		    						</div>
+			    				</div>
+		    				</div>
+		    			</div>
+		    			<div class="col-md-6 col-lg-6 ftco-animate">
+		    				<div class="blog-entry">
+		    					<div class="img img-big img-big-2 d-flex align-items-end" style="background-image: url(images/image_3.jpg);">
+		    						<div class="overlay"></div>
+		    						<div class="text">
+		    							<span class="subheading">Lifestyle</span>
+		    							<h3><a href="#">White and red ceramic plate</a></h3>
+		    							<p class="mb-0"><a href="#" class="btn-custom">Read More <span class="icon-arrow_forward ml-4"></span></a></p>
+		    						</div>
+			    				</div>
+		    				</div>
+		    			</div>
+        		</div>
+        	</div>
+        	<div class="col-md-3">
+        		<div class="sidebar-wrap pt-4">
+	            <div class="sidebar-box categories text-center ftco-animate">
+			          <h2 class="heading mb-4">Categories</h2>
+			          <ul class="category-image">
+			          	<li>
+			          		<a href="foods.html" class="img d-flex align-items-center justify-content-center text-center" style="background-image: url(images/category-1.jpg);">
+			          			<div class="text">
+			          				<h3>Foods</h3>
+			          			</div>
+			          		</a>
+			          	</li>
+			          	<li>
+			          		<a href="lifestyle.html" class="img d-flex align-items-center justify-content-center text-center" style="background-image: url(images/category-2.jpg);">
+			          			<div class="text">
+			          				<h3>Lifestyle</h3>
+			          			</div>
+			          		</a>
+			          	</li>
+			          	<li>
+			          		<a href="#" class="img d-flex align-items-center justify-content-center text-center" style="background-image: url(images/category-2.jpg);">
+			          			<div class="text">
+			          				<h3>Others</h3>
+			          			</div>
+			          		</a>
+			          	</li>
+			          </ul>
+	        		</div>
+            </div>
+        	</div>
+        </div>
+    	</div>
+    </section>
